f519cc748194519802e5f60eadc34ec12bd30e35
Tantely Randria <tantely.tnr@gmail.com>
2018-12-03 11:48:27 +0100

    Création d'une interface Fly, ajout méthode, implémentation.

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,36 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff(){
+    	this.flying = true;
+    	System.out.println(this.getName() + " takes off in the sky.");
+    }
+
+    @Override
+    public int ascend(int meters){
+    	this.altitude += meters;
+    	System.out.println(this.getName() + " flies upward, altitude : " + this.altitude);
+    	return this.altitude;
+    }
+
+    @Override
+    public int descend(int meters){
+    	this.altitude -= meters;
+    	System.out.println(this.getName() + " flies downward, altitude : " + this.altitude);
+	return this.altitude;
+    }
+
+    @Override
+    public void land() {
+        if(this.altitude > 1){
+		System.out.println(this.getName() + " is too high, it can't lands.");
+        } else {
+        	this.altitude = 0;
+        	this.flying = false;
+        	System.out.println(this.getName() + " lands on the ground.");
+        }
     }
 }
